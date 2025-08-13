d606ff2cae51e79beda7b92acf8ef8d1cd06cd8d
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-13 23:37:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
   //your JS code here.
-}
+	if (!str) return false;
 
+const emailRegex = /^[\w.-]+@[\w-]+(\.[\w-]+)*\.[A-Za-z]{2,}$/;
+
+    return emailRegex.test(str);
+}
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
